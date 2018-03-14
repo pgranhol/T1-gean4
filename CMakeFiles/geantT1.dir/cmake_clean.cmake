90b690574501a642ae646d49a28227ab69ee8497
pgranhol <pgranhol@gmail.com>
2018-03-14 23:54:15 +0200

    now all files

---
+++ CMakeFiles/geantT1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/geantT1.dir/T1main.cc.o"
+  "CMakeFiles/geantT1.dir/src/T1PhysicsList.cc.o"
+  "CMakeFiles/geantT1.dir/src/T1PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/geantT1.dir/src/T1RunAction.cc.o"
+  "CMakeFiles/geantT1.dir/src/T1ActionInitialization.cc.o"
+  "CMakeFiles/geantT1.dir/src/T1DetectorConstruction.cc.o"
+  "geantT1.pdb"
+  "geantT1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/geantT1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
